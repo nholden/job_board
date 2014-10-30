e3fdae6bbb99a5b68a9afbdc4614c77b91c3e44c
Nick Holden <nholden@bu.edu>
2014-10-30 07:49:54 -0400

    Add employer_controller tests for creating password

--- spec/controllers/employers_controller_spec.rb
+++ spec/controllers/employers_controller_spec.rb
@@ -8,14 +8,29 @@ RSpec.describe EmployersController, :type => :controller do
     end
   end
 
-  describe "creating a new employer" do
-    it "creates the new employer" do
+  describe "POST #create" do
+    it "creates a new employer" do
       expect{ post :create, employer: FactoryGirl.attributes_for(:employer) }.to change(Employer,:count).by(1)
     end
 
-    it "redirects to home page upon save" do
+    it "redirects to home page" do
       post :create, employer: FactoryGirl.attributes_for(:employer)
       expect(response).to redirect_to root_url
+    end
+
+    def try_password(password)
+      post :create, employer: FactoryGirl.attributes_for(:employer)
+      Employer.find_by(name: 'Boeing').try(:authenticate, 'password')
+    end
+
+    it "does not reject a valid password" do
+      try_password("password")
+      expect(response).to be_truthy
+    end
+
+    it "rejects an invalid password" do
+      try_password("not_password")
+      expect(response).to be_false
     end
   end
 end
