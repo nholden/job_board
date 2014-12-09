8ce4e07b11e6f9a460a5d87f925dd79b2a1b56f6
Nick Holden <nholden@bu.edu>
2014-12-08 20:53:14 -0500

    Add create method to jobs controller

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -5,4 +5,20 @@ class JobsController < ApplicationController
   def new
     @job = Job.new
   end
+
+  def create
+    @job = Job.new(job_params)
+    if @job.save
+      flash[:notice] = "Created job."
+      redirect_to root_url
+    else
+      flash[:error] = @job.errors.full_messages[0]
+      render :action => 'new'
+    end
+  end
+
+  private
+    def job_params
+      params.require(:job).permit(:title, :type, :location, :experience, :majors, :description, :url, :instructions, :deadline, :salary)
+    end
 end
