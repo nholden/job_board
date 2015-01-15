b1d45a854d3360c735802eff48f4e7a8fcb89b08
Nick Holden <nholden@bu.edu>
2015-01-15 15:40:15 -0500

    Finish moving job terms to own table

--- features/step_definitions/filter_jobs_steps.rb
+++ features/step_definitions/filter_jobs_steps.rb
@@ -1,6 +1,6 @@
 When(/^he filters for full time jobs$/) do
   visit('/')
-  select('Full time permanent', :from => 'q_term_eq')
+  select('Full time permanent', :from => 'q_term_id_eq')
   click_button('Search')
 end
 
@@ -12,13 +12,13 @@ end
 
 When(/^he filters for internships requiring some college$/) do
   visit('/')
-  select('Internship', :from => 'q_term_eq')
+  select('Internship', :from => 'q_term_id_eq')
   select('Some college', :from => 'q_experience_id_eq')
   click_button('Search')
 end
 
 When(/^he removes all filters$/) do
-  select('', :from => 'q_term_eq')
+  select('', :from => 'q_term_id_eq')
   select('', :from => 'q_experience_id_eq')
   click_button('Search')
 end
