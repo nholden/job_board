200165fce5b048e890881b3ac8113968050501fd
Nick Holden <nholden@bu.edu>
2015-03-04 07:41:11 -0500

    Add functionality to destroy_and_reassign_jobs method in experience controller

--- app/models/experience.rb
+++ app/models/experience.rb
@@ -2,5 +2,10 @@ class Experience < ActiveRecord::Base
   has_many :jobs
 
   def destroy_and_reassign_jobs
+    self.jobs.each do |job|
+      job.experience = Experience.find_or_create_by(label: 'Unassigned')
+      job.save
+    end
+    self.destroy
   end
 end
