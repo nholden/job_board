c415b05a951fd86805dc8e272b9ba7017341e4e6
Nick Holden <nholden@bu.edu>
2015-03-27 07:11:08 -0400

    Move all of experience model to manageable concern

--- app/models/concerns/manageable.rb
+++ app/models/concerns/manageable.rb
@@ -28,4 +28,26 @@ module Manageable
       self.destroy
     end
   end
+
+  module ClassMethods
+    def reposition(hash)
+      if hash.values.uniq.length != hash.values.length
+        false
+      else
+        self.update_all position: nil
+        new_order = hash.sort_by { |id, position| position }
+        new_order.each do |id_position_pair|
+          self.find(id_position_pair[0]).save if self.exists?(id_position_pair[0])
+        end
+      end
+    end
+
+    def refresh_positions
+      id_position_pairs = {}
+      self.all.each do |managed|
+        id_position_pairs[managed.id] = managed.position
+      end
+      self.reposition(id_position_pairs)
+    end
+  end
 end
