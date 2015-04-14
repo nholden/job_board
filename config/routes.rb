6d93e8a69d303a4a712ecd372c980a2358b2b127
Nick Holden <nholden@bu.edu>
2015-04-14 17:21:49 -0400

    Remove /users/new route

--- config/routes.rb
+++ config/routes.rb
@@ -27,7 +27,7 @@ Rails.application.routes.draw do
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
   resources :jobs
-  resources :users
+  resources :users, only: [:create, :edit, :update, :index, :destroy]
 
   # Example resource route with options:
   #   resources :products do
