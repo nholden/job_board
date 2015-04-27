b296c458425faad6ea6f46968832055d3e295113
Nick Holden <nholden@bu.edu>
2015-04-27 07:41:32 -0400

    Add first applications controller spec

--- spec/controllers/applications_controller_spec.rb
+++ spec/controllers/applications_controller_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe ApplicationsController, :type => :controller do
+  describe "POST #create" do
+    before (:each) do
+      @application_attributes = FactoryGirl.attributes_for(:application)
+    end
 
+    it "creates a new application" do
+      expect{post :create, application: @application_attributes}.to
+        change(Application.all, :count).by(1)
+    end
+  end 
 end
