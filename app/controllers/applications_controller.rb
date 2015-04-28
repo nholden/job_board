80ff9701e3dd0665dcb753329c5805da326ebf4b
Nick Holden <nholden@bu.edu>
2015-04-28 07:12:42 -0400

    Add create method to applications controller

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationsController < ApplicationController
+  def create
+    @application = Application.create
+    flash[:notice] = "Application successfully submitted."
+    redirect_to root_url
+  end
 end
--- spec/controllers/applications_controller_spec.rb
+++ spec/controllers/applications_controller_spec.rb
@@ -7,8 +7,17 @@ RSpec.describe ApplicationsController, :type => :controller do
     end
 
     it "creates a new application" do
-      expect{post :create, application: @application_attributes}.to
-        change(Application.all, :count).by(1)
+      expect{post :create}.to change(Application.all, :count).by(1)
+    end
+
+    it "redirects to root_url" do
+      post :create
+      expect(response).to redirect_to(root_url)
+    end
+
+    it "sends a flash" do
+      post :create
+      expect(flash[:notice]).to eql("Application successfully submitted.")
     end
   end 
 end
