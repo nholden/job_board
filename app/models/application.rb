45951bc75543582441995e6f931ea5b2ab5292d5
Nick Holden <nholden@bu.edu>
2015-04-29 19:27:50 -0400

    Set default status for new applications

--- app/models/application.rb
+++ app/models/application.rb
@@ -1,4 +1,9 @@
 class Application < ActiveRecord::Base
   belongs_to :user
   belongs_to :job
+
+  before_save :default_values
+  def default_values
+    self.status ||= "Submitted"
+  end
 end
--- spec/models/application_spec.rb
+++ spec/models/application_spec.rb
@@ -8,4 +8,9 @@ RSpec.describe Application, :type => :model do
   it "should belong_to job" do
     expect(Application.reflect_on_association(:job).macro).to eq(:belongs_to)
   end
+
+  it "should have a default status of 'Submitted'" do
+    @application = FactoryGirl.create(:application, status: nil)
+    expect(@application.status).to eql("Submitted")
+  end
 end
