9f0120fa35f59128aab772e4614d7e30603bbd1a
Nick Holden <nholden@bu.edu>
2015-05-06 07:23:50 -0400

    Prevent uploading resumes larger than 5MB

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ActiveRecord::Base
   has_attached_file :resume
   validates_attachment_content_type :resume, :content_type => 'application/pdf'
   validates_attachment_file_name :resume, :matches => /pdf\Z/
+  validates_attachment_size :resume, :less_than => 5.megabytes
   validates :email, presence: true, uniqueness: true, format: { with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i }
   validates :role, presence: true
   belongs_to :role
