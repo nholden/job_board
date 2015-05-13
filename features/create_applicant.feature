d6f8c2dfcea8127e705942895131be4859efc379
Nick Holden <nholden@bu.edu>
2015-05-13 19:52:03 -0400

    Prevent delete resume button from appearing on user form if user form hasn't been saved

--- features/create_applicant.feature
+++ features/create_applicant.feature
@@ -13,3 +13,12 @@ Feature: Create applicant
     Then he should see "Created applicant account"
     Then he should see "Log out"
     Then he should not see "Post new job"
+
+  Scenario: Fail to signup with no name
+    Given an administrator exists
+    Given the user is not logged in
+    When he visits the jobs page
+    When he clicks "Sign up"
+    When he clicks "Applicant"
+    When he fills in the signup applicant form with no name
+    Then he should see "Name can't be blank"
--- features/step_definitions/create_applicant_steps.rb
+++ features/step_definitions/create_applicant_steps.rb
@@ -16,6 +16,16 @@ When(/^he fills in the signup applicant form$/) do
   fill_in('user_email', :with => 'job@seeker.net')
   fill_in('user_password', :with => 'password1')
   fill_in('user_password_confirmation', :with => 'password1')
+  attach_file(:user_resume, File.join(Rails.root, 'features', 'files', 'Example_Resume_v01.pdf'))
+  click_button('Create account')
+end
+
+When(/^he fills in the signup applicant form with no name$/) do
+  visit('/signup/applicant')
+  fill_in('user_email', :with => 'job@seeker.net')
+  fill_in('user_password', :with => 'password1')
+  fill_in('user_password_confirmation', :with => 'password1')
+  attach_file(:user_resume, File.join(Rails.root, 'features', 'files', 'Example_Resume_v01.pdf'))
   click_button('Create account')
 end
 
