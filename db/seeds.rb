7528b80bd833298594848c324049b4ae954f3f0c
Nick Holden <nholden@bu.edu>
2015-06-08 07:40:40 -0400

    Seed with sample employers and jobs

--- db/seeds.rb
+++ db/seeds.rb
@@ -40,3 +40,31 @@ User.create!(name:                  "Administrator",
                password:              "password",
                password_confirmation: "password")
 end
+
+10.times do |n|
+  name = Faker::Company.name
+  User.create!(name:                  name,
+               email:                 Faker::Internet.email(name),
+               role:                  Role.find_or_create_by(label: 'employer'),
+               password:              "password",
+               password_confirmation: "password")
+end
+
+50.times do |n|
+  description = ""
+  Faker::Lorem.paragraphs.each do |paragraph|
+    description += "#{paragraph}\n\n"
+  end
+
+  employers = Role.find_by(label:'employer').users
+
+  Job.create!(title:       Faker::Name.title,
+              location:    Faker::Address.city,
+              majors:      Faker::Hacker.noun,
+              description: description,
+              deadline:    Faker::Date.forward(60),
+              salary:      "$#{Faker::Number.number(2)}K per year",
+              user:        employers.offset(rand(employers.count)).first,
+              experience:  Experience.offset(rand(Experience.count)).first,
+              term:        Term.offset(rand(Term.count)).first)
+end
