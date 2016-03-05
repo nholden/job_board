0e31b048e22ee1cf056eed785c11911f9e593110
Nick Holden <nholden@bu.edu>
2016-03-05 16:51:14 -0500

    Clean up applications_controller create method

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -10,12 +10,9 @@ class ApplicationsController < ApplicationController
   end
 
   def create
-    if logged_in? and current_user.id == application_params[:user_id].to_i
-      @application = Application.new(application_params)
-      job_title = Job.find(application_params[:job_id]).title
-      if @application.save
-        flash[:notice] = "Application to '#{job_title}' successfully submitted."
-      end
+    application = Application.new(application_params)
+    if current_user == application.user and application.save
+      flash[:notice] = "Application to '#{application.job.title}' successfully submitted."
     else
       flash[:error] = "You must be logged in as an applicant to apply."
     end
